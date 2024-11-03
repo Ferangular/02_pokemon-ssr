b363a64c627b830cfcd43ae360ec530b9d015492
Ferangular <firthunsnpm@gmail.com>
2024-11-03 12:08:01 +0100

    feat:  actualizacion version angular y instalamos ssr  y ng add @angular/ssr

--- src/app/pages/pricing/pricing.component.ts
+++ src/app/pages/pricing/pricing.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {Meta, Title} from '@angular/platform-browser';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-pricing',
@@ -15,6 +16,8 @@ export default class PricingComponent implements OnInit {
   private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+    console.log(isPlatformBrowser(this.platform));
+    // no usar esta forma
     // if (isPlatformBrowser(this.platform)) {
     //   document.title = 'Pricing Page';
     // }
